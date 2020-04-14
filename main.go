86b7ca10f653c914f83a9220f880689ba1b98f79
Hsing-Hui Hsu <hhhsu1@gmail.com>
2020-04-13 21:55:56 -0700

    add special fields and fix sorting

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go/token"
 	"gotags/parser"
+	"sort"
 )
 
 func main() {
@@ -15,7 +16,20 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	tags := []string{}
 	for _, f := range files {
-		parser.Generate(f, fset)
+		parser.Generate(f, fset, &tags)
 	}
+
+	// Print special fields
+	fmt.Println("!_TAG_FILE_FORMAT\t2\t/extended format/")
+	fmt.Println("!_TAG_FILE_SORTED\t1\t/sorted/")
+	fmt.Println("!_TAG_PROGRAM_AUTHOR\tHsing-Hui Hsu\thhhsu1@gmail.com")
+	fmt.Println("!_TAG_PROGRAM_NAME\tgotags\t")
+	fmt.Println("!_TAG_PROGRAM_URL\thttps://github.com/SoManyHs/gotags")
+	sort.Strings(tags)
+	for _, tag := range tags {
+		fmt.Printf(tag)
+	}
+
 }
